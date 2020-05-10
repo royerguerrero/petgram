8c1614ae5f73cd1de18ea7fa775fbcfc82be4072
Royer G <royjuni3431@gmail.com>
2020-05-09 20:15:28 -0500

    Modelo de usuario

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
+"""Posts models"""
+
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    """User Model"""
+
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    bio = models.TextField(blank=True)
+
+    birthdate = models.DateTimeField(blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
