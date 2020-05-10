34b1822998f7d0e23e803b1c67cfac42a1ef3838
Royer G <royjuni3431@gmail.com>
2020-05-09 19:11:28 -0500

    App de posts

--- petgram/urls.py
+++ petgram/urls.py
@@ -2,10 +2,12 @@
 # Django
 from django.urls import path
 # Local
-from petgram import views
+from petgram import views as local_views
+from posts import views as posts_views
 
 urlpatterns = [
-    path('hi/<str:name>/<int:age>/', views.hi),
-    path('sort-numbers/', views.sorted_numbers),
-    path('hello-world/', views.hello_world),
+    path('hi/<str:name>/<int:age>/', local_views.hi),
+    path('sort-numbers/', local_views.sorted_numbers),
+    path('hello-world/', local_views.hello_world),
+    path('posts/', posts_views.list_posts)
 ]
